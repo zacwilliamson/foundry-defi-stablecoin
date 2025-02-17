4a428e649c82d84db008284c1e620c2c4d8c37f6
supersakana <zacwilliamson@icloud.com>
2025-02-17 16:20:08 -0600

    updated DecentralizedStableCoin constructor with ownable

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -38,31 +38,35 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
  * This is the contract meant to be governed by DSCEngine. This contract is just the ERC20 implementation of our stablecoin system.
  */
 contract DecentralizedStableCoin is ERC20Burnable, Ownable {
-    error DecentralizedStableCoin__MustBeMoreThanZero();
+    error DecentralizedStableCoin__AmountMustBeMoreThanZero();
     error DecentralizedStableCoin__BurnAmountExceedsBalance();
     error DecentralizedStableCoin__NotZeroAddress();
 
-    constructor() ERC20("DecentralizedStableCoin", "DSC") {}
+    constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(msg.sender) {}
 
     // Note: We overide #burn and #mint parent functions to configure our own access control
-    function burn(uint256 _amount) external override onlyOwner {
+    function burn(uint256 _amount) public override onlyOwner {
         uint256 balance = balanceOf(msg.sender);
         if (_amount <= 0) {
-            revert DecentralizedStableCoin__MustBeMoreThanZero();
+            revert DecentralizedStableCoin__AmountMustBeMoreThanZero();
         }
         if (balance < _amount) {
             revert DecentralizedStableCoin__BurnAmountExceedsBalance();
         }
         super.burn(_amount);
     }
-    function mint(address _to, uint256 _amount) external onlyOwner returns(bool){
-        if(_to == address(0)){
+
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
             revert DecentralizedStableCoin__NotZeroAddress();
         }
-        if(_amount <= 0){
-            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__AmountMustBeMoreThanZero();
         }
-        _mint(_to, amount)
+        _mint(_to, _amount);
         return true;
     }
 }
