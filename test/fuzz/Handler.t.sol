eb7bd7c7a653a7fb0d93190549ab47bd2aec9af0
supersakana <zacwilliamson@icloud.com>
2025-02-20 11:49:21 -0600

    started basic invariant/fuzz test setup

---
+++ test/fuzz/Handler.t.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+
+// Narrows down the way we call functions
+
+pragma solidity ^0.8.18;
---
+++ test/fuzz/Invariants.t.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+
+// Contains properties of the system that should always hold
+
+pragma solidity 0.8.19;
+
+import {Test, console} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Invariants is StdInvariant, Test {
+    DeployDSC deployer;
+    DSCEngine dsce;
+    DecentralizedStableCoin dsc;
+    HelperConfig config;
+    address weth;
+    address wbtc;
+
+    function setUp() external {
+        deployer = new DeployDSC();
+        (dsc, dsce, config) = deployer.run();
+        (,, weth, wbtc,) = config.activeNetworkConfig();
+        targetContract(address(dsce));
+    }
+
+    function invariant_protocolMustHaveMoreValueThanTotalSupply() public view {
+        uint256 totalSupply = dsc.totalSupply();
+        uint256 totalWethDeposited = IERC20(weth).balanceOf(address(dsce));
+        uint256 totalWbtcDeposited = IERC20(wbtc).balanceOf(address(dsce));
+
+        uint256 wethValue = dsce.getUsdValue(weth, totalWethDeposited);
+        uint256 wbtcValue = dsce.getUsdValue(wbtc, totalWbtcDeposited);
+
+        console.log("Weth Value: ", wethValue);
+        console.log("Wbtc Value: ", wbtcValue);
+        console.log("Total Supply: ", totalSupply);
+
+        assert(wethValue + wbtcValue >= totalSupply);
+    }
+}
